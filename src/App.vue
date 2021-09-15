6128d3ce01f58a0cc3d62251538e2ce178fdf6ac
user <togogo234@gmail.com>
2021-09-15 05:07:54 -0700

    add Header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
+<v-app>
   <div id="app">
+    <v-app-bar color="blue">
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+    
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    
     <router-view/>
   </div>
+</v-app>
 </template>
 
 <style lang="scss">
@@ -17,16 +24,29 @@
   color: #2c3e50;
 }
 
+
 #nav {
   padding: 30px;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #42b983;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #2c3e50;
     }
   }
 }
 </style>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+} 
+</script>
